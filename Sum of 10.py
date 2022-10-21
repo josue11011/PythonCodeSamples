28ba0ed5906e6fc13bb64ea94f587c3f49398cc7
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-10-21 14:08:00 -0500

    Update Sum of 10.py

--- Sum of 10.py
+++ Sum of 10.py
@@ -4,5 +4,7 @@
 #Date: October 16,2022
 #Purpose of the program:To print the sum of numbers 1-10
 #=======================================
+#sum = 1+2+3+4+5+6+7+8+9+10
+#print("The sum of 1 thru 10 is {}. ".format(sum))
 
 print(1+2+3+4+5+6+7+8+9+10)
