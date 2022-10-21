e53484f7937658486ab09809b32a7d730a70860a
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-10-21 14:09:15 -0500

    Update Josue greeting.py

--- Josue greeting.py
+++ Josue greeting.py
@@ -4,5 +4,7 @@
 #Date: October 16,2022
 #Purpose of the program:To print a greeting with your name
 #=======================================
-
+#ask name to a user.
+#name = input("What is your name? ")
+#print("Hello " + name)
 print('Hello, Josue!')
